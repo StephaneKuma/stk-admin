6b5482cb25c6a97a0d7553df71a8828331047ef8
Stéphane Kuma <kumastephane@gmail.com>
2023-07-01 00:19:34 +0000

    Vuesax installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,18 @@
 import './bootstrap';
+
 import { createApp } from 'vue';
+import Vuesax from 'vuesax3'
 import App from '@/App.vue';
 import HelloWorld from '@/components/HelloWorld.vue';
 import router from '@/router';
 
+import "vuesax3/dist/vuesax.css";
+
 const app = createApp(App);
+
+app.use(Vuesax)
+app.use(router)
 
 app.component('helloWorld', HelloWorld);
 
-app.use(router)
 app.mount('#app');
